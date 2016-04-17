988faea8e0966c7706cbe86a16fc9a096897458e
wjabraham <wjabraham@gmail.com>
2016-04-17 15:55:34 -0400

    Fix clipping issue with legend in PNG 3 and 4

--- plot3.R
+++ plot3.R
@@ -22,11 +22,14 @@ power$Voltage <- as.numeric(power$Voltage)
 with(power, plot(DateTime, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering"))
 with(power, lines(DateTime, power$Sub_metering_2, type = "l", col = "red"))
 with(power, lines(DateTime, power$Sub_metering_3, type = "l", col = "blue"))
-legend("topright", 
+legend("topright",
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        lty = c(1, 1, 1), 
-       col = c("black", "red", "blue"))
+       col = c("black", "red", "blue"),
+       cex = 0.6, 
+       inset=c(0.175,0), 
+       bty = "n")
 
-# Output to PNG file
+# Output to PNG NULL
 dev.copy(png, filename = "plot3.png", width = 480, height = 480, units = "px")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -31,10 +31,13 @@ with(power, plot(DateTime, Voltage, type = "l"))
 with(power, plot(DateTime, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering"))
 with(power, lines(DateTime, power$Sub_metering_2, type = "l", col = "red"))
 with(power, lines(DateTime, power$Sub_metering_3, type = "l", col = "blue"))
-legend("topright", 
+legend("topright",
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        lty = c(1, 1, 1), 
-       col = c("black", "red", "blue"))
+       col = c("black", "red", "blue"),
+       cex = 0.6, 
+       inset=c(0.175,0), 
+       bty = "n")
 
 # Plot 4
 with(power, plot(DateTime, Global_reactive_power, type = "l"))
